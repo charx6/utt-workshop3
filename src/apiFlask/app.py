9b65f64626681b2cd2d3b2ae671f15af2de81899
charx6 <charlesbonaly@hotmail.fr>
2021-12-09 15:09:02 +0100

    apifonctionne

--- src/apiFlask/app.py
+++ src/apiFlask/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:blue'>Farah on te voit</h1>"
 
 
-
+if __name__ == "__main__":
+    app.run(debug = False, host = "0.0.0.0", port=5000)
